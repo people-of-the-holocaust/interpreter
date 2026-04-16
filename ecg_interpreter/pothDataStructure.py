4f237067d5bca42c65a0d29187e2feaed172696e
LuuT <149102520+LuuT90@users.noreply.github.com>
2026-04-16 16:06:21 -0500

    Attempt to add functionality to add insert into database
    Add the ability to insert into database. However need to change the password from public to encrypted.

--- ecg_interpreter/pothDataStructure.py
+++ ecg_interpreter/pothDataStructure.py
@@ -4,6 +4,8 @@ import bycrypt
 
 #TODO Add Getters and Setters to data structure.
 
+hashedPass = "$2a$12$4pwVb9bxNCsByFDbwpXXQOBwMvjq.gNFArdovKlno8yHq91AvmcQS"
+
 #Encyclopedia class
 class Encyclopedia:
     #List of Volumes, will always be Volumes class
@@ -22,6 +24,24 @@ class Encyclopedia:
         for volume in self.volumesList:
             outputActions.extend(volume.getActions)
         return outputActions
+    
+    def insert_into_database(self, inputpass):
+        try:
+            bcrypt.checkpw(inputpass, hashedPass)
+            encActions = self.getActions
+            #Fix this where password to be a hashed bcrypt password AND update password away from this test password.
+            connect = psycopg2.connect("dbname='postgres', user='postgres', host='db.elkjpkawrounaqjwzbjo.supabase.co', password='inputpass'")
+
+            with connect.cursor as curs:
+                for actions in encActions:
+                #Add to execute for ids, description, and so on. May need to rework an 
+                    curs.execute("INSERT INTO Activity (Person Subj ID, Person Obj ID, Action, details, Place ID) VALUES (%s, %s, %s, %s, %s)",
+                             (actions.personSubjID, actions.personObjID, actions.action, actions.details, actions.placeId))
+            connect.commit()
+            curs.close()
+            connect.close()
+        except:
+            print("Error, passwords do not match")
     
 #Page class (Used in Encyclopedia)
 class Volume:
@@ -68,24 +88,3 @@ class Action:
     action = None
     details = None
     placeID = None
-
-
-#REMEMBER encrypt so that others cannot get access to the database without authorization.
-#Must be an Encyclopedia value
-def insert_into_database(root):
-    encActions = root.getActions
-    #Fix this where password is the password for account
-    connect = psycopg2.connect("dbname='postgres', user='01a76196-f218-4028-888e-c7b310493720', host='db.elkjpkawrounaqjwzbjo.supabase.co', password='dummyPass")
-
-    with connect.cursor as curs:
-        for actions in encActions:
-            #Add to execute for ids, description, and so on.
-            curs.execute("INSERT INTO Activity (Person Subj ID, Person Obj ID, Action, details, Place ID) VALUES (%s, %s, %s, %s, %s)",
-                         (actions.personSubjID, actions.personObjID, actions.action, actions.details, actions.placeId))
-
-
-#People: fName, afName, mName, lName, alName, title, dates, status, organizations, gender
-#Places: Name, Type, Subtype, Current Country, Latitude, Longitude, Location Accuracy
-#Activity: description, date
-#for data in encylopediaData:
-#    dbEditor.execute("INSERT INTO people ()")
