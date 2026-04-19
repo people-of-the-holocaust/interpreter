9fda867c5e37da31cc94a86f57b35d86d59bb843
Tania Mishra <tmishra3@argolis.mscsnet.mu.edu>
2026-04-19 15:09:27 -0500

    main method fully working, need to connect to database

--- ecg_interpreter/__main__.py
+++ ecg_interpreter/__main__.py
@@ -25,9 +25,13 @@ def main():
     raw_ppl_names = get_raw_names(session, name_index_url)
     ppl_df = create_ppl_table(raw_ppl_names)
 
+    print(ppl_df.head())
+
     # set up place_table
     place_csv_path = files("ecg_interpreter").joinpath("tables/place_table.csv")
     plc_df = pd.read_csv(place_csv_path)
+
+    print(plc_df.head())
 
     # create encyclopedia node
     ecg_node = Encyclopedia()
@@ -35,36 +39,46 @@ def main():
     print("STARTING VOLUME SCRAPING")
 
     for vnum, vurl in vol_urls.items():
+        print("VOLUME:", vnum)
+
         # create volume node
         curr_vol = Volume(vnum, vurl)
         ecg_node.addVolume(curr_vol)
         # get content articles from volume
         article_links = scrape_vol(vurl, plc_df, session, vnum)
+        print("NUMBER OF ARTICLES:", len(article_links))
         # loop over each article
         i = 0
         for lid, link in article_links.items():
-            # TESTING - ONLY LOOK AT FIRST 20 ARTICLES
-            if i >= 20:
+            # TESTING - ONLY LOOK AT FIRST 10 ARTICLES
+            if i >= 10:
                 break
-            place, body = get_article_content((base_article_url + link), session)
-            # link is formatted as '/document/####'
-            docNum = int(link.split("/")[-1])
-            # create article node
-            curr_article = Article(place, body, docNum)
-            curr_vol.addArticle(curr_article)
-            # get sentences in article
-            sentences = sent_tokenize(body)
-            # loop over each sentence
-            for sent in sentences:
-                pids = is_key(sent, ppl_df)
-                if type(pids) == list and len(pids) > 0:
-                    # create sentence node
-                    curr_sent = Sentence(sent, pids)
-                    curr_article.addSent(curr_sent)
-                    # loop over each pid found in key sentence
-                    for pid in pids:
-                        # get action node
-                        curr_sent.addAction(get_person_action(sent, pid, ppl_df, lid))
+            try:
+                place, body = get_article_content((base_article_url + link), session)
+
+                # link is formatted as '/document/####'
+                doc_num = int(link.split("/")[-1])
+                print("Scraped", doc_num)
+
+                # create article node
+                curr_article = Article(place, body, doc_num)
+                curr_vol.addArticle(curr_article)
+                # get sentences in article
+                sentences = sent_tokenize(body)
+                # loop over each sentence
+                for sent in sentences:
+                    pids = is_key(sent, ppl_df)
+                    if type(pids) == list and len(pids) > 0:
+                        print("Sentence is key:", sent)
+                        # create sentence node
+                        curr_sent = Sentence(sent, pids)
+                        curr_article.addSent(curr_sent)
+                        # loop over each pid found in key sentence
+                        for pid in pids:
+                            # get action node
+                            curr_sent.addAction(get_person_action(sent, pid, ppl_df, lid))
+            except:
+                continue
             i += 1
 
     final_tree = print_ecg(ecg_node)
