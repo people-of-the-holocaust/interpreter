f72e9bacf703ec0c8d0f31bed36f18a0b449ac9a
Tania Mishra <43151673+TaniaMishra@users.noreply.github.com>
2026-05-05 09:45:32 -0500

    final code and data (#52)
    * putting actions into csv at end
    
    * ran on 40 articles, created activity table
    
    * adding actions after each volume
    
    * added error log output
    
    * reran test articles with error log
    
    * sectioning off code that doesn't need to rerun every time
    
    * sectioning off code that doesn't need to rerun every time pt 2
    
    * ran tests to save all article links
    
    * finished sectioning off code that doesn't need to rerun every time
    
    * tried rerunning to create new activity log, unsure if worked
    
    * reset activity table and cleaned up print statements
    
    * changed to run tests for 20 articles
    
    * ran on 20 articles per volume
    
    * prep for run on full ecg
    
    * prepped for tests with db connection
    
    * test run with 20 articles worked in db
    
    * Scraping data for entire Encyclopedia
    
    * full run through, db worked for half
    
    ---------
    
    Co-authored-by: Tania Mishra <tmishra3@argolis.mscsnet.mu.edu>

--- ecg_interpreter/__main__.py
+++ ecg_interpreter/__main__.py
@@ -6,86 +6,104 @@ from nltk.tokenize import sent_tokenize
 from importlib.resources import files
 
 def main():
-    print("IN MAIN")
+    # LEVEL: ENCYCLOPEDIA
 
-    # urls
+    # SET UP URLS
     base_article_url = "https://muse.jhu.edu"
-    name_index_url = 'https://muse.jhu.edu/ushmm/index/names'
     vol_urls = {
         1: "https://muse.jhu.edu/resource_group/13",
         2: "https://muse.jhu.edu/resource_group/59",
         3: "https://muse.jhu.edu/resource_group/91",
         4: "https://muse.jhu.edu/resource_group/111"
     }
+    # necessary for get_raw_names function
+    # name_index_url = 'https://muse.jhu.edu/ushmm/index/names'
 
     # create session to save cookies
     session = requests.Session()
 
-    # set up people_table
-    raw_ppl_names = get_raw_names(session, name_index_url)
-    ppl_df = create_ppl_table(raw_ppl_names)
+    # SET UP people_table
+    # option 1: scrape ECG people index, clean names, sort into table
+    # raw_ppl_names = get_raw_names(session, name_index_url)
+    # ppl_df = create_ppl_table(raw_ppl_names)
+    # option 2: load in table with first column as index
+    ppl_csv_path = files("ecg_interpreter").joinpath("tables/people_table.csv")
+    ppl_df = pd.read_csv(ppl_csv_path, index_col=0)
 
-    print(ppl_df.head())
+    # SET UP place_table
+    # necessary for scrape_vol function
+    # place_csv_path = files("ecg_interpreter").joinpath("tables/place_table.csv")
+    # plc_df = pd.read_csv(place_csv_path)
 
-    # set up place_table
-    place_csv_path = files("ecg_interpreter").joinpath("tables/place_table.csv")
-    plc_df = pd.read_csv(place_csv_path)
+    # SET UP activity dataframe
+    # activity_df = pd.DataFrame()
 
-    print(plc_df.head())
+    # SET UP error log
+    with open("error_log.txt", "w") as f:
+        f.write("Exceptions occured while processing the following articles:\n")
 
-    # create encyclopedia node
+    print("Completed initial set up")
+
+    # CREATE encyclopedia node
     ecg_node = Encyclopedia()
 
-    print("STARTING VOLUME SCRAPING")
-
     for vnum, vurl in vol_urls.items():
-        print("VOLUME:", vnum)
-
-        # create volume node
+        # CREATE volume node
         curr_vol = Volume(vnum, vurl)
         ecg_node.addVolume(curr_vol)
-        # get links for content articles from volume page
-        article_links = scrape_vol(vurl, plc_df, session, vnum)
-        print("NUMBER OF ARTICLES:", len(article_links))
-        # loop over each article
-        i = 0
-        for lid, link in article_links.items():
-            # TESTING - ONLY LOOK AT FIRST 10 ARTICLES
-            if i >= 10:
-                break
+
+        # get links for content articles
+        # option 1: scrape volume page, save into df
+        # article_links = scrape_vol(vurl, plc_df, session, vnum)
+        # option 2: load in table with first column as index
+        file_name = "tables/vol" + str(vnum) + "_article_links.csv"
+        article_links_csv_path = files("ecg_interpreter").joinpath(file_name)
+        article_links = pd.read_csv(article_links_csv_path, index_col=0)
+
+        for row in article_links.itertuples():
+            lid = row.LID
+            link = row.doc_link
+            # link is formatted as '/document/####'
+            doc_num = int(link.split("/")[-1])
             try:
                 # scrape article content
                 place, body = get_article_content((base_article_url + link), session)
-
-                # link is formatted as '/document/####'
-                doc_num = int(link.split("/")[-1])
-                print("Scraped", doc_num)
-
-                # create article node
+                # CREATE article node
                 curr_article = Article(place, body, doc_num)
                 curr_vol.addArticle(curr_article)
-                # get sentences in article
+
+                # split article body into sentences
                 sentences = sent_tokenize(body)
-                # loop over each sentence
                 for sent in sentences:
                     # check if sent is KEY, get pids from sent
                     pids = is_key(sent, ppl_df)
                     if type(pids) == list and len(pids) > 0:
-                        print("Sentence is key:", sent)
-                        # create sentence node
+                        # CREATE sentence node
                         curr_sent = Sentence(sent, pids)
                         curr_article.addSent(curr_sent)
-                        # loop over each pid found in key sentence
                         for pid in pids:
-                            # get action node
+                            # CREATE action node
                             curr_sent.addAction(get_person_action(sent, pid, ppl_df, lid))
             except:
-                print("Exception occured - skipped article", link)
+                with open("error_log.txt", "a") as f:
+                    f.write(f"{doc_num}\n")
                 continue
-            i += 1
+        # get all actions for this volume, save into activity_df
+        # vol_actions = curr_vol.getActions()
+        # vol_acts_as_dicts = [vars(act) for act in vol_actions]
+        # activity_df = pd.concat([activity_df, pd.DataFrame(vol_acts_as_dicts)], ignore_index=True)
+        # activity_csv_path = files("ecg_interpreter").joinpath("tables/activity_table.csv")
+        # activity_df.to_csv(activity_csv_path)
+        # get all actions for this volume into the database
+        # curr_vol.insertIntoDatabase()
+        print("Updated activities for volume", vnum)
 
-    final_tree = print_ecg(ecg_node)
-    with open("tree_output.txt", "w") as f:
-        f.write(final_tree)
-    # print(final_tree)
+    ecg_node.insertIntoDatabase()
+    # export activity_df to csv
+    # activity_csv_path = files("ecg_interpreter").joinpath("tables/activity_table.csv")
+    # activity_df.to_csv(activity_csv_path)
+
+    # final_tree = print_ecg(ecg_node)
+    # with open("tree_output.txt", "w") as f:
+        # f.write(final_tree)
     return 0
